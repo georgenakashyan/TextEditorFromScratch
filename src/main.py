bd4a8b5c338f4131732bfb50fad164749eb34990
George N. - PC <91430969+Gevoooo@users.noreply.github.com>
2024-05-21 17:51:57 -0400

    feat: add key presses and paint on screen

--- src/main.py
+++ src/main.py
@@ -19,11 +19,46 @@ class CustomTextEdit(QWidget):
         self.setFont(self.font)
 
     def keyPressEvent(self, event):
-        self.text.append(event.text()) #BUG: Only appends, does not take into account cursor pos
-        print("You typed: " + event.text()) #INFO: Remove later
+        row, col = self.cursor_pos
+        key = event.key()
+        not_first_row = row > 0
+        not_last_row = row < len(self.text) - 1
         
-    def update_blinking(self):
-        ...
+        if key == Qt.Key_Backspace:
+            ...
+        elif key == Qt.Key_Return or key == Qt.Key_Enter:
+            ...
+        elif key == Qt.Key_Left:
+            ...
+        elif key == Qt.Key_Right:
+            ...
+        elif key == Qt.Key_Up and not_first_row:
+            ...
+        elif key == Qt.Key_Down and not_last_row:
+            ...
+        else:
+            self.text[row] = self.text[row][:col] + event.text() + self.text[row][col:]
+            self.cursor_pos = (row, col+1)
+        
+        self.update()
+        
+    def paintEvent(self, event):
+        qp = QPainter(self)
+        qp.setFont(self.font)
+        metrics = qp.fontMetrics()
+        row_height = metrics.height()
+        for row, line in enumerate(self.text):
+            qp.drawText(0, (row+1) * row_height, line)
+        
+        if self.cursor_visible:
+            row, col = self.cursor_pos
+            x = metrics.horizontalAdvance(self.text[row][:col])
+            y = row * row_height
+            qp.drawLine(x, y, x, y + row_height)
+    
+    def blink_cursor(self):
+        self.cursor_visible = not self.cursor_visible
+        self.update()
 
 def main():
     app = QApplication(sys.argv)
